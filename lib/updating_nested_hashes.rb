f125b1a72af162c9ef8e4a2ec23f166a2c00a2cc
Tej Charfi <charfi@usc.edu>
2020-08-16 19:51:14 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -50,7 +50,8 @@ def add_characters
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      }
+      }, 
+      :additional_characters => [{name: "Prince Escalus"}, {name: "Apothecary"}]
    }
 
    # Write code below that does the following programmatically:
